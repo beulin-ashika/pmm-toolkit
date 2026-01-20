b67a019b7c43c946a78b8d751ef4551a8e36eb88
Beulin Ashika <118802861+beulin-ashika@users.noreply.github.com>
2026-01-20 18:11:52 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ProductMarketingToolkit from './ProductMarketingToolkit'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProductMarketingToolkit />
+  </React.StrictMode>,
+)
